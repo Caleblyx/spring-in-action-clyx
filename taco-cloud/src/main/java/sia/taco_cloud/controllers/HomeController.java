3ae26e2731c6aa9a92771f8b5a6e7cc8a754a76b
Admin <caleblyx@outlook.com>
2024-07-13 18:37:05 +0800

    write an endpoint which serves up a homepage

---
+++ taco-cloud/src/main/java/sia/taco_cloud/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package sia.taco_cloud.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class HomeController {
+    
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+    
+}
